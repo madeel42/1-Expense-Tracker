6248779e44f63358445cfd7d715220ee315aa38a
madeel42 <madeelakhtar204@gmail.com>
2021-05-03 16:14:17 +0500

    4:14pm

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useState } from 'react'
+import { createContext, useReducer } from 'react'
 import { AppReducer } from './Appreducer'
 import *  as AllTypes from './Types'
 ///Create initial State
